6dd3f3637cb0b60b84a329deb0a582d13b1c99e8
Ibarahona73 <igyt2015@gmail.com>
2025-05-22 12:31:14 -0500

    admin,tablas

--- TiendaOnline/admin.py
+++ TiendaOnline/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Producto, Pedido
+from .models import Producto, Pedido, Usuario, ISVPais, PedidoDetalle
 
 
 # Reegister los modelos en el admin de Django
 # Esto permite que los modelos sean administrados a través de la interfaz de administración de Django
 admin.site.register(Producto)
 admin.site.register(Pedido)
+admin.site.register(Usuario)
+admin.site.register(ISVPais)
+admin.site.register(PedidoDetalle)
