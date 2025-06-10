e7e10f9768483f33444a511c6282783241bcb0c4
Ibarahona73 <igyt2015@gmail.com>
2025-06-10 16:25:52 -0500

    models

--- TiendaOnline/models.py
+++ TiendaOnline/models.py
@@ -484,7 +484,8 @@ class CarritoTemp(models.Model):
 
     def delete(self, *args, **kwargs):
         """Devuelve el stock al eliminar el registro"""
-        if not self.expirado:
+        # Solo devolver el stock si no está expirado y hay cantidad temporal
+        if not self.expirado and self.cantidad_temp > 0:
             self.producto.cantidad_en_stock += self.cantidad_temp
             self.producto.save()
         super().delete(*args, **kwargs)
